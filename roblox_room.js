3a698e286312a8cc6c286d0157919ce9a4231acb
SpondonArafin <82678481+SpondonArafin@users.noreply.github.com>
2021-08-02 21:21:46 -0400

    Add files via upload

---
+++ roblox_room.js
@@ -0,0 +1,73 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBb9SXnOEkZbApv0WyG5GXCl3eALDMaDGA",
+    authDomain: "roblox-fanbase.firebaseapp.com",
+    databaseURL: "https://roblox-fanbase-default-rtdb.firebaseio.com",
+    projectId: "roblox-fanbase",
+    storageBucket: "roblox-fanbase.appspot.com",
+    messagingSenderId: "644000008795",
+    appId: "1:644000008795:web:ff084d7d498d565f6c8d7e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  
+  
+  
+  
+  
+  user_name = localStorage.getItem("user_name");
+  document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+  
+  
+  
+  
+  function addRoom() {
+    room_name = document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+      purpose: "adding room name"
+    });
+    localStorage.setItem("room_name", room_name);
+    window.location = "roblox_page.html";
+  }
+  
+  
+  
+  
+  function getData() {
+    firebase.database().ref("/").on('value', function (snapshot) {
+      document.getElementById("output").innerHTML = "";
+      snapshot.forEach(function (childSnapshot) {
+        childKey = childSnapshot.key;
+        Room_names = childKey;
+        console.log("Room Name - " + Room_names);
+        row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+        document.getElementById("output").innerHTML += row;
+      });
+    });
+  }
+    
+  
+  
+  
+  getData();
+  
+  
+  
+  
+  function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name", name);
+    window.location = "roblox_page.html";
+  }
+  
+  
+  
+  
+  
+  
+  function logout() {
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location = "index.html";
+  }
